66a7478b11a4fd0a6c65b39cee0c1e1425e1ae42
Evan BOISSONNOT <evan@boissonnot.fr>
2023-11-21 15:25:35 +0100

    feat: popin

--- appentissage react/sw-biblio/src/App.tsx
+++ appentissage react/sw-biblio/src/App.tsx
@@ -1,13 +1,22 @@
+import { Alert } from 'react-bootstrap'
 import './App.css'
 import { ListPeople } from './features/people/components/ListPeople'
 import { Header } from './shared/ui/components/Header'
 
 function App() {
+  const popin = <Alert key='default' variant='warning'>Youpi</Alert>
+  let displayPopin = false;
+
+  const displayComponent = () => {
+    displayPopin = !displayPopin;
+  }
+
   return (
     <>
       <Header></Header>
+      { displayPopin && popin }
       <h1>Ma bibliothèque Star wars</h1>
-      <ListPeople></ListPeople>
+      <ListPeople displayPopin={displayComponent}></ListPeople>
     </>
   )
 }
--- appentissage react/sw-biblio/src/features/people/components/ListPeople/index.tsx
+++ appentissage react/sw-biblio/src/features/people/components/ListPeople/index.tsx
@@ -1,19 +1,29 @@
 import { TableUIPeople } from "../TableUIPeople";
 import { People } from "../../models/people"
-import { Alert } from "react-bootstrap";
+import { Alert, Button } from "react-bootstrap";
+
+export type ListPeopleProps = {
+    displayPopin: () => void;
+}
 
 /**
  * Head component
  */
-export const ListPeople = () => {
+export const ListPeople = (props: ListPeopleProps) => {
     console.info('ListPeople', this);
     const people: People = [{id: 1, prenom: 'Luke'}];
     const warningPasElements =  <Alert key='warning' variant='warning'>Aucune personne disponible</Alert>
+
+    // const executeClick = () => {
+    //     props.displayPopin();
+    // }
 
     return (
         <>
            { people.length === 0 && warningPasElements }
            { people.length > 0 && <TableUIPeople items={people} ></TableUIPeople> }
+
+           <Button variant="info" onClick={props.displayPopin} >Affichage popin</Button>
         </>
     )
 }
